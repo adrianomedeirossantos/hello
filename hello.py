796e4d05d8e18bd12fbf4b1c5c7840582fcbbf18
Adriano Medeiros dos Santos <adriano.santos@gousto.co.uk>
2025-03-28 20:27:02 +0000

    Test

--- hello.py
+++ hello.py
@@ -1,9 +1,4 @@
 def say_hello():
     return "Hello, world!"
 
-# test_hello.py
-import pytest
-from hello import say_hello
-
-def test_say_hello():
-    assert say_hello() == "Hello, world!"
+say_hello()
